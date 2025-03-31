e0b0437a92b7bb8ff4adc7b51da4a82c165f967f
yfigueira <figueirapy@gmail.com>
2025-03-31 22:19:03 +0200

    setup comment ci

--- post-service/src/test/java/org/example/postservice/PostServiceApplicationTests.java
+++ post-service/src/test/java/org/example/postservice/PostServiceApplicationTests.java
@@ -3,14 +3,11 @@ package org.example.postservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class PostServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
 	}
 
 }
